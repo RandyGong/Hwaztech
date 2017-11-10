7b284046b3ce04e7c059eea021b5ebbc698d6801
Randy Jichang Gong <attackwc@163.com>
2017-11-10 19:28:46 +0800

    update

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,8 @@ var app = angular.module('hwaztechApp', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', { templateUrl: 'view/main.html' })
-        .when('/developers', { templateUrl: 'view/developers.html', })
-        .when('/printers', { template: '这是打印机页面' })
+        .when('/developers', { templateUrl: 'view/developers.html' })
+        .when('/case-studies', { templateUrl: 'view/case-studies.html' })
+        .when('/methodology', { templateUrl: 'view/methodology.html' })
         .otherwise({ redirectTo: '/' });
 }]);
