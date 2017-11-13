2c077e6c8d6cbe0d11eb7abf7f2c24e978a4b430
Randy Jichang Gong <attackwc@163.com>
2017-11-13 18:58:34 +0800

    add bottom row

--- scripts/appController.js
+++ scripts/appController.js
@@ -2,8 +2,22 @@
 
     'use strict';
 
-    var appController = function () {        
+    var appController = function ($scope, $location) {     
+        $scope.field = 'approach'; 
+        
+        $scope.$watch(function () {
+            return $location.search();
+        }, function (newValue, oldValue) {            
+            var location = angular.lowercase($location.$$url);            
+            if (location) {
+                if (location.indexOf('methodology') !== -1) {
+                    $scope.field = 'methodology'; 
+                }else {
+                    $scope.field = 'approach'; 
+                }
+            }
+        });
     }
 
-    app.controller('appController', [appController]);
+    app.controller('appController', ['$scope', '$location', appController]);
 })();
