3445c16d98ff1f1fec6336f20aab4410e2fb7d7d
Randy Jichang Gong <attackwc@163.com>
2018-02-09 00:54:58 +0800

    update responsive

--- scripts/appController.js
+++ scripts/appController.js
@@ -2,18 +2,22 @@
 
     'use strict';
 
-    var appController = function ($scope, $location) {     
-        $scope.field = 'approach'; 
-        
+    var appController = function ($scope, $location) {
+        $scope.field = 'approach';
+        var bodyWidth = window.document.body.clientWidth;
+        $scope.isNarrow = bodyWidth < 460;
+        $scope.isPad = bodyWidth >= 460 && bodyWidth < 1024;
+        $scope.isFull = bodyWidth >= 1024;
+
         $scope.$watch(function () {
             return $location.search();
-        }, function (newValue, oldValue) {            
-            var location = angular.lowercase($location.$$url);            
+        }, function (newValue, oldValue) {
+            var location = angular.lowercase($location.$$url);
             if (location) {
                 if (location.indexOf('methodology') !== -1) {
-                    $scope.field = 'methodology'; 
-                }else {
-                    $scope.field = 'approach'; 
+                    $scope.field = 'methodology';
+                } else {
+                    $scope.field = 'approach';
                 }
             }
         });
